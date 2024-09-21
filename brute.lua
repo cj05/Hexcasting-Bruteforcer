c1ae931b03a73d1caf0c2b5b2baccff0d0b9222e
Cj <cjcoolsuper@gmail.com>
2024-09-21 17:25:24 +0700

    Update brute.lua

--- brute.lua
+++ brute.lua
@@ -1,6 +1,33 @@
 staff = peripheral.wrap("left")
-print(arg[1])
-data = fs.open(arg[1] or "list.hexbrute","r").readAll()
+--print(arg[1])
+if arg[1] == "-o" then
+    print("Entering Online Mode")
+    local repo = "https://raw.githubusercontent.com/cj05/Hexcasting-Bruteforcer/refs/heads/main/registry.json"
+    local response = http.get(repo)
+    local resdata = response.readAll()
+    local repoinfo = textutils.unserializeJSON(resdata)
+    local wait = true
+    local selection
+    while wait do
+        print("Available Pattern Datas")
+        for i,_ in pairs(repoinfo) do
+        print(i)
+        end
+        local sel = read()
+        if repoinfo[sel] ~= nil then
+            selection = repoinfo[sel]
+            wait = false
+        end
+        term.setCursorPos(1,1)
+        term.clear()
+        print("Invalid Selection, Please Try Again")
+    end
+    response = http.get(selection)
+    data = response.readAll()
+    print(data)
+else
+    data = fs.open(arg[1] or "list.hexbrute","r").readAll()
+end
 patterns = textutils.unserializeJSON(data)
 index = 1
 total = #patterns
